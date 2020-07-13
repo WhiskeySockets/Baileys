99e109130db57931b6c289240ab0bb99c7402797
Adhiraj <adhirajsingh1001@gmail.com>
2020-07-13 14:18:53 +0530

    Update WAClient.ts

--- src/WAClient/WAClient.ts
+++ src/WAClient/WAClient.ts
@@ -4,3 +4,4 @@ export { WhatsAppWebMessages as WAClient }
 export * from './Constants'
 export * from './Utils'
 export * from '../WAConnection/Constants'
+export { Browsers } from '../WAConnection/Utils'
