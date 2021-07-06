d4189c3d000e3bf58514343bb3724cfd0aaca041
Adhiraj Singh <adhirajsingh1001@gmail.com>
2021-07-06 16:14:20 +0530

    Update version

--- src/WAConnection/0.Base.ts
+++ src/WAConnection/0.Base.ts
@@ -32,7 +32,7 @@ const logger = pino({ prettyPrint: { levelFirst: true, ignore: 'hostname', trans
 
 export class WAConnection extends EventEmitter {
     /** The version of WhatsApp Web we're telling the servers we are */
-    version: [number, number, number] = [2, 2119, 6]
+    version: [number, number, number] = [2, 2123, 8]
     /** The Browser we're telling the WhatsApp Web servers we are */
     browserDescription: [string, string, string] = Utils.Browsers.baileys ('Chrome')
     /** Metadata like WhatsApp id, name set on WhatsApp etc. */
