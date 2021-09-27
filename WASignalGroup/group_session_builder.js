12105a17eee4a9ace47f77f5b4d2a356e7d3b433
Adhiraj Singh <adhirajsingh1001@gmail.com>
2021-09-27 12:48:57 +0530

    groups key generation bug fix

--- WASignalGroup/group_session_builder.js
+++ WASignalGroup/group_session_builder.js
@@ -1,7 +1,7 @@
 //const utils = require('../../common/utils');
 const SenderKeyDistributionMessage = require('./sender_key_distribution_message');
 
-const keyhelper = require("libsignal/src/keyhelper");
+const keyhelper = require("./keyhelper");
 class GroupSessionBuilder {
   constructor(senderKeyStore) {
     this.senderKeyStore = senderKeyStore;
@@ -21,31 +21,26 @@ class GroupSessionBuilder {
 
   // [{"senderKeyId":1742199468,"senderChainKey":{"iteration":0,"seed":"yxMY9VFQcXEP34olRAcGCtsgx1XoKsHfDIh+1ea4HAQ="},"senderSigningKey":{"public":""}}]
   async create(senderKeyName) {
-    try {
-      const senderKeyRecord = await this.senderKeyStore.loadSenderKey(senderKeyName);
-      //console.log('GroupSessionBuilder create session', senderKeyName, senderKeyRecord);
+    const senderKeyRecord = await this.senderKeyStore.loadSenderKey(senderKeyName);
+    //console.log('GroupSessionBuilder create session', senderKeyName, senderKeyRecord);
 
-      if (senderKeyRecord.isEmpty()) {
-        const keyId = keyhelper.generateSenderKeyId();
-        const senderKey = keyhelper.generateSenderKey();
-        const signingKey = keyhelper.generateSenderSigningKey();
+    if (senderKeyRecord.isEmpty()) {
+      const keyId = keyhelper.generateSenderKeyId();
+      const senderKey = keyhelper.generateSenderKey();
+      const signingKey = keyhelper.generateSenderSigningKey();
 
-        senderKeyRecord.setSenderKeyState(keyId, 0, senderKey, signingKey);
-        await this.senderKeyStore.storeSenderKey(senderKeyName, senderKeyRecord);
-      }
-
-      const state = senderKeyRecord.getSenderKeyState();
-
-      return new SenderKeyDistributionMessage(
-        state.getKeyId(),
-        state.getSenderChainKey().getIteration(),
-        state.getSenderChainKey().getSeed(),
-        state.getSigningKeyPublic()
-      );
-    } catch (e) {
-      //console.log(e.stack);
-      throw new Error(e);
+      senderKeyRecord.setSenderKeyState(keyId, 0, senderKey, signingKey);
+      await this.senderKeyStore.storeSenderKey(senderKeyName, senderKeyRecord);
     }
+
+    const state = senderKeyRecord.getSenderKeyState();
+
+    return new SenderKeyDistributionMessage(
+      state.getKeyId(),
+      state.getSenderChainKey().getIteration(),
+      state.getSenderChainKey().getSeed(),
+      state.getSigningKeyPublic()
+    );
   }
 }
 module.exports = GroupSessionBuilder;
