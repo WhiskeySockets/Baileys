e954fc00539a38c99bb62a3551be0d744f679f18
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-01-22 11:37:39 +0530

    lint: examples

--- Example/example-legacy.ts
+++ Example/example-legacy.ts
@@ -1,6 +1,6 @@
-import P from "pino"
-import { Boom } from "@hapi/boom"
-import { makeWALegacySocket, DisconnectReason, AnyMessageContent, delay, useSingleFileLegacyAuthState, makeInMemoryStore } from '../src'
+import { Boom } from '@hapi/boom'
+import P from 'pino'
+import { AnyMessageContent, delay, DisconnectReason, makeInMemoryStore, makeWALegacySocket, useSingleFileLegacyAuthState } from '../src'
 
 // the store maintains the data of the WA connection in memory
 // can be written out to a file & read from it
@@ -8,7 +8,7 @@ const store = makeInMemoryStore({ logger: P().child({ level: 'debug', stream: 's
 store.readFromFile('./baileys_store.json')
 // save every 10s
 setInterval(() => {
-    store.writeToFile('./baileys_store.json')
+	store.writeToFile('./baileys_store.json')
 }, 10_000)
 
 const { state, saveState } = useSingleFileLegacyAuthState('./auth_info.json')
@@ -16,61 +16,61 @@ const { state, saveState } = useSingleFileLegacyAuthState('./auth_info.json')
 // start a connection
 const startSock = () => {
     
-    const sock = makeWALegacySocket({
-        logger: P({ level: 'debug' }),
-        printQRInTerminal: true,
-        auth: state
-    })
-    store.bind(sock.ev)
+	const sock = makeWALegacySocket({
+		logger: P({ level: 'debug' }),
+		printQRInTerminal: true,
+		auth: state
+	})
+	store.bind(sock.ev)
 
-    const sendMessageWTyping = async(msg: AnyMessageContent, jid: string) => {
-        await sock.presenceSubscribe(jid)
-        await delay(500)
+	const sendMessageWTyping = async(msg: AnyMessageContent, jid: string) => {
+		await sock.presenceSubscribe(jid)
+		await delay(500)
 
-        await sock.sendPresenceUpdate('composing', jid)
-        await delay(2000)
+		await sock.sendPresenceUpdate('composing', jid)
+		await delay(2000)
 
-        await sock.sendPresenceUpdate('paused', jid)
+		await sock.sendPresenceUpdate('paused', jid)
 
-        await sock.sendMessage(jid, msg)
-    }
+		await sock.sendMessage(jid, msg)
+	}
     
-    sock.ev.on('messages.upsert', async m => {
-        if(m.type === 'append' || m.type === 'notify') {
-            console.log(JSON.stringify(m, undefined, 2))
-        }
+	sock.ev.on('messages.upsert', async m => {
+		if(m.type === 'append' || m.type === 'notify') {
+			console.log(JSON.stringify(m, undefined, 2))
+		}
         
-        const msg = m.messages[0]
-        if(!msg.key.fromMe && m.type === 'notify') {
-            console.log('replying to', m.messages[0].key.remoteJid)
-            await sock!.chatRead(msg.key, 1)
-            await sendMessageWTyping({ text: 'Hello there!' }, msg.key.remoteJid)
-        }
+		const msg = m.messages[0]
+		if(!msg.key.fromMe && m.type === 'notify') {
+			console.log('replying to', m.messages[0].key.remoteJid)
+			await sock!.chatRead(msg.key, 1)
+			await sendMessageWTyping({ text: 'Hello there!' }, msg.key.remoteJid)
+		}
         
-    })
+	})
 
-    sock.ev.on('messages.update', m => console.log(JSON.stringify(m, undefined, 2)))
-    sock.ev.on('presence.update', m => console.log(m))
-    sock.ev.on('chats.update', m => console.log(m))
-    sock.ev.on('contacts.update', m => console.log(m))
+	sock.ev.on('messages.update', m => console.log(JSON.stringify(m, undefined, 2)))
+	sock.ev.on('presence.update', m => console.log(m))
+	sock.ev.on('chats.update', m => console.log(m))
+	sock.ev.on('contacts.update', m => console.log(m))
 
-    sock.ev.on('connection.update', (update) => {
-        const { connection, lastDisconnect } = update
-        if(connection === 'close') {
-            // reconnect if not logged out
-            if((lastDisconnect.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut) {
-                startSock()
-            } else {
-                console.log('connection closed')
-            }
-        }
+	sock.ev.on('connection.update', (update) => {
+		const { connection, lastDisconnect } = update
+		if(connection === 'close') {
+			// reconnect if not logged out
+			if((lastDisconnect.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut) {
+				startSock()
+			} else {
+				console.log('connection closed')
+			}
+		}
         
-        console.log('connection update', update)
-    })
-    // listen for when the auth credentials is updated
-    sock.ev.on('creds.update', saveState)
+		console.log('connection update', update)
+	})
+	// listen for when the auth credentials is updated
+	sock.ev.on('creds.update', saveState)
 
-    return sock
+	return sock
 }
 
 startSock()
