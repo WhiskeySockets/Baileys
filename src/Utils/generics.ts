f83ee95abbfb683831828f70e700f3159a43a201
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-03-21 23:30:35 +0530

    chore: update browser versions

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -16,8 +16,8 @@ const PLATFORM_MAP = {
 }
 
 export const Browsers = {
-	ubuntu: browser => ['Ubuntu', browser, '18.04'] as [string, string, string],
-	macOS: browser => ['Mac OS', browser, '10.15.3'] as [string, string, string],
+	ubuntu: browser => ['Ubuntu', browser, '20.0.04'] as [string, string, string],
+	macOS: browser => ['Mac OS', browser, '10.15.7'] as [string, string, string],
 	baileys: browser => ['Baileys', browser, '4.0.0'] as [string, string, string],
 	/** The appropriate browser based on your OS & release */
 	appropriate: browser => [ PLATFORM_MAP[platform()] || 'Ubuntu', browser, release() ] as [string, string, string]
