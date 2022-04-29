846abbf7b9f3f08bef810e4240c939dc72497e1d
✬Ravindu Manoj✬ ✓ <88134633+ravindu01manoj@users.noreply.github.com>
2022-04-29 18:32:58 +0530

    catch errors in groupParticipantsUpdate (#1545)
    * catch errors in groupParticipantsUpdate
    
    * catch errors in groupParticipantsUpdate

--- src/Socket/groups.ts
+++ src/Socket/groups.ts
@@ -99,7 +99,9 @@ export const makeGroupsSocket = (config: SocketConfig) => {
 			)
 			const node = getBinaryNodeChild(result, action)
 			const participantsAffected = getBinaryNodeChildren(node!, 'participant')
-			return participantsAffected.map(p => p.attrs.jid)
+			return participantsAffected.map(p => {
+				return { status: p.attrs.error || 200, jid: p.attrs.jid }
+			})
 		},
 		groupUpdateDescription: async(jid: string, description?: string) => {
 			const metadata = await groupMetadata(jid)
