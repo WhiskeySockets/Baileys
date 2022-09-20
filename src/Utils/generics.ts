28afa96cb9f9f8212afa96265cd0f168c3a1ba45
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-09-20 12:15:33 +0530

    chore: make arg optional in fetchLatestBaileysVersion

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -218,7 +218,7 @@ export const printQRIfNecessaryListener = (ev: BaileysEventEmitter, logger: Logg
  * utility that fetches latest baileys version from the master branch.
  * Use to ensure your WA connection is always on the latest version
  */
-export const fetchLatestBaileysVersion = async(options: AxiosRequestConfig<any>) => {
+export const fetchLatestBaileysVersion = async(options: AxiosRequestConfig<any> = { }) => {
 	const URL = 'https://raw.githubusercontent.com/adiwajshing/Baileys/master/src/Defaults/baileys-version.json'
 	try {
 		const result = await axios.get<{ version: WAVersion }>(
