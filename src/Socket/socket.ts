3977444bc5b1f701c41014dd107549f0d8e7e817
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-10-16 15:25:10 +0530

    fix: handle errors in validateconnection

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -425,7 +425,14 @@ export const makeSocket = ({
 	}
 
 	ws.on('message', onMessageRecieved)
-	ws.on('open', validateConnection)
+	ws.on('open', async() => {
+		try {
+			await validateConnection()
+		} catch(err) {
+			logger.error({ err }, 'error in validating connection')
+			end(err)
+		}
+	})
 	ws.on('error', error => end(
 		new Boom(
 			`WebSocket Error (${error.message})`,
