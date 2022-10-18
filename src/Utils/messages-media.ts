c59952f2eeb07e80fafa598b22fd9e3f5f1f8657
kaatt <10688522+kaatt@users.noreply.github.com>
2022-10-18 09:31:32 +0530

    fix extractImageThumb threw with webpack (#2266)
    * fix extractImageThumb threw with webpack
    
    * Update messages-media.ts
    
    * Update src/Utils/messages-media.ts
    
    * fix: typescript errors
    
    Co-authored-by: Adhiraj Singh <adhirajsingh1001@gmail.com>

--- src/Utils/messages-media.ts
+++ src/Utils/messages-media.ts
@@ -96,7 +96,7 @@ export const extractImageThumb = async(bufferOrFilePath: Readable | Buffer | str
 	}
 
 	const lib = await getImageProcessingLibrary()
-	if('sharp' in lib) {
+	if('sharp' in lib && lib.sharp?.default) {
 		const img = lib.sharp!.default(bufferOrFilePath)
 		const dimensions = await img.metadata()
 
@@ -111,7 +111,7 @@ export const extractImageThumb = async(bufferOrFilePath: Readable | Buffer | str
 				height: dimensions.height,
 			},
 		}
-	} else {
+	} else if('jimp' in lib && lib.jimp) {
 		const { read, MIME_JPEG, RESIZE_BILINEAR, AUTO } = lib.jimp
 
 		const jimp = await read(bufferOrFilePath as any)
@@ -127,6 +127,8 @@ export const extractImageThumb = async(bufferOrFilePath: Readable | Buffer | str
 			buffer,
 			original: dimensions
 		}
+	} else {
+		throw new Boom('No image processing library available')
 	}
 }
 
