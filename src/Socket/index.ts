e9500aba1dfdcd9ee41d0c11457a4788f49e9afe
Samuel <34555296+Flam3rboy@users.noreply.github.com>
2023-01-29 12:56:40 +0100

    revert structure commit

--- src/Socket/index.ts
+++ src/Socket/index.ts
@@ -1,10 +1,10 @@
 import { DEFAULT_CONNECTION_CONFIG } from '../Defaults'
 import { UserFacingSocketConfig } from '../Types'
-import { Business } from './business'
+import { makeBusinessSocket as _makeSocket } from './business'
 
 // export the last socket layer
 const makeWASocket = (config: UserFacingSocketConfig) => (
-	new Business({
+	_makeSocket({
 		...DEFAULT_CONNECTION_CONFIG,
 		...config
 	})
