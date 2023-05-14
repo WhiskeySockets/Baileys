da96212c2a4fccfec18bdb51dfc76731e74c2178
Rafael Garcia <rafaeljosegarciasuarez@gmail.com>
2023-05-14 12:51:53 -0500

    chore(labels): update example script

--- Example/example.ts
+++ Example/example.ts
@@ -3,7 +3,7 @@ import NodeCache from 'node-cache'
 import makeWASocket, { AnyMessageContent, delay, DisconnectReason, fetchLatestBaileysVersion, getAggregateVotesInPollMessage, makeCacheableSignalKeyStore, makeInMemoryStore, proto, useMultiFileAuthState, WAMessageContent, WAMessageKey } from '../src'
 import MAIN_LOGGER from '../src/Utils/logger'
 
-const logger = MAIN_LOGGER.child({ })
+const logger = MAIN_LOGGER.child({})
 logger.level = 'trace'
 
 const useStore = !process.argv.includes('--no-store')
@@ -23,7 +23,7 @@ setInterval(() => {
 }, 10_000)
 
 // start a connection
-const startSock = async() => {
+const startSock = async () => {
 	const { state, saveCreds } = await useMultiFileAuthState('baileys_auth_info')
 	// fetch latest version of WA Web
 	const { version, isLatest } = await fetchLatestBaileysVersion()
@@ -49,7 +49,7 @@ const startSock = async() => {
 
 	store?.bind(sock.ev)
 
-	const sendMessageWTyping = async(msg: AnyMessageContent, jid: string) => {
+	const sendMessageWTyping = async (msg: AnyMessageContent, jid: string) => {
 		await sock.presenceSubscribe(jid)
 		await delay(500)
 
@@ -65,15 +65,15 @@ const startSock = async() => {
 	// efficiently in a batch
 	sock.ev.process(
 		// events is a map for event name => event data
-		async(events) => {
+		async (events) => {
 			// something about the connection changed
 			// maybe it closed, or we received all offline message or connection opened
-			if(events['connection.update']) {
+			if (events['connection.update']) {
 				const update = events['connection.update']
 				const { connection, lastDisconnect } = update
-				if(connection === 'close') {
+				if (connection === 'close') {
 					// reconnect if not logged out
-					if((lastDisconnect?.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut) {
+					if ((lastDisconnect?.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut) {
 						startSock()
 					} else {
 						console.log('Connection closed. You are logged out.')
@@ -84,28 +84,37 @@ const startSock = async() => {
 			}
 
 			// credentials updated -- save them
-			if(events['creds.update']) {
+			if (events['creds.update']) {
 				await saveCreds()
 			}
 
-			if(events.call) {
+			if (events['labels.association']) {
+				console.log(events['labels.association'])
+			}
+
+
+			if (events['labels.edit']) {
+				console.log(events['labels.edit'])
+			}
+
+			if (events.call) {
 				console.log('recv call event', events.call)
 			}
 
 			// history received
-			if(events['messaging-history.set']) {
+			if (events['messaging-history.set']) {
 				const { chats, contacts, messages, isLatest } = events['messaging-history.set']
 				console.log(`recv ${chats.length} chats, ${contacts.length} contacts, ${messages.length} msgs (is latest: ${isLatest})`)
 			}
 
 			// received a new message
-			if(events['messages.upsert']) {
+			if (events['messages.upsert']) {
 				const upsert = events['messages.upsert']
 				console.log('recv messages ', JSON.stringify(upsert, undefined, 2))
 
-				if(upsert.type === 'notify') {
-					for(const msg of upsert.messages) {
-						if(!msg.key.fromMe && doReplies) {
+				if (upsert.type === 'notify') {
+					for (const msg of upsert.messages) {
+						if (!msg.key.fromMe && doReplies) {
 							console.log('replying to', msg.key.remoteJid)
 							await sock!.readMessages([msg.key])
 							await sendMessageWTyping({ text: 'Hello there!' }, msg.key.remoteJid!)
@@ -115,15 +124,15 @@ const startSock = async() => {
 			}
 
 			// messages updated like status delivered, message deleted etc.
-			if(events['messages.update']) {
+			if (events['messages.update']) {
 				console.log(
 					JSON.stringify(events['messages.update'], undefined, 2)
 				)
 
-				for(const { key, update } of events['messages.update']) {
-					if(update.pollUpdates) {
+				for (const { key, update } of events['messages.update']) {
+					if (update.pollUpdates) {
 						const pollCreation = await getMessage(key)
-						if(pollCreation) {
+						if (pollCreation) {
 							console.log(
 								'got poll update, aggregation: ',
 								getAggregateVotesInPollMessage({
@@ -136,25 +145,25 @@ const startSock = async() => {
 				}
 			}
 
-			if(events['message-receipt.update']) {
+			if (events['message-receipt.update']) {
 				console.log(events['message-receipt.update'])
 			}
 
-			if(events['messages.reaction']) {
+			if (events['messages.reaction']) {
 				console.log(events['messages.reaction'])
 			}
 
-			if(events['presence.update']) {
+			if (events['presence.update']) {
 				console.log(events['presence.update'])
 			}
 
-			if(events['chats.update']) {
+			if (events['chats.update']) {
 				console.log(events['chats.update'])
 			}
 
-			if(events['contacts.update']) {
-				for(const contact of events['contacts.update']) {
-					if(typeof contact.imgUrl !== 'undefined') {
+			if (events['contacts.update']) {
+				for (const contact of events['contacts.update']) {
+					if (typeof contact.imgUrl !== 'undefined') {
 						const newUrl = contact.imgUrl === null
 							? null
 							: await sock!.profilePictureUrl(contact.id!).catch(() => null)
@@ -165,7 +174,7 @@ const startSock = async() => {
 				}
 			}
 
-			if(events['chats.delete']) {
+			if (events['chats.delete']) {
 				console.log('chats deleted ', events['chats.delete'])
 			}
 		}
@@ -174,7 +183,7 @@ const startSock = async() => {
 	return sock
 
 	async function getMessage(key: WAMessageKey): Promise<WAMessageContent | undefined> {
-		if(store) {
+		if (store) {
 			const msg = await store.loadMessage(key.remoteJid!, key.id!)
 			return msg?.message || undefined
 		}
