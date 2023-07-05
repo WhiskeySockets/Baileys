d645800ff471566d0875278da7ef18ce7cd78071
SamuelScheit <github@samuelscheit.com>
2023-07-06 00:35:20 +0200

    fix: registration + update mobile version

--- src/Defaults/index.ts
+++ src/Defaults/index.ts
@@ -19,9 +19,9 @@ export const PHONE_CONNECTION_CB = 'CB:Pong'
 
 export const WA_DEFAULT_EPHEMERAL = 7 * 24 * 60 * 60
 
-export const MOBILE_TOKEN = Buffer.from('0a1mLfGUIBVrMKF1RdvLI5lkRBvof6vn0fD2QRSM4174c0243f5277a5d7720ce842cc4ae6')
+export const MOBILE_TOKEN = Buffer.from('0a1mLfGUIBVrMKF1RdvLI5lkRBvof6vn0fD2QRSMc46ab726e8fb8c66811b0b95c014e262')
 export const MOBILE_REGISTRATION_ENDPOINT = 'https://v.whatsapp.net/v2'
-export const MOBILE_USERAGENT = 'WhatsApp/2.22.24.81 iOS/15.3.1 Device/Apple-iPhone_7'
+export const MOBILE_USERAGENT = 'WhatsApp/2.23.12.75 iOS/15.3.1 Device/Apple-iPhone_7'
 export const REGISTRATION_PUBLIC_KEY = Buffer.from([
 	5, 142, 140, 15, 116, 195, 235, 197, 215, 166, 134, 92, 108, 60, 132, 56, 86, 176, 97, 33, 204, 232, 234, 119, 77,
 	34, 251, 111, 18, 37, 18, 48, 45,
