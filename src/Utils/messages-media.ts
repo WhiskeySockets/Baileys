5693fa060d0fcf0a39af43a469f228a28fbb5568
Codeboss <131376628+codebossdev@users.noreply.github.com>
2023-07-08 02:44:13 +0000

    This is exactly the fix

--- src/Utils/messages-media.ts
+++ src/Utils/messages-media.ts
@@ -209,7 +209,7 @@ export async function getAudioDuration(buffer: Buffer | string | Readable) {
  */
 export async function getAudioWaveform(bodyPath: string, logger?: Logger) {
 	try {
-		const audioDecode = (await import('audio-decode')).default;
+		const audioDecode = await import('audio-decode');
 		const fileBuffer = await fs.readFile(bodyPath)
 		const audioBuffer = await audioDecode(fileBuffer)
 
