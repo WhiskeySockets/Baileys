d1b57cf990679807a89db322c5057974e256f1ca
azudin <azudin.daem@gmail.com>
2023-12-07 23:37:06 +0800

    Update chats.ts

--- src/Socket/chats.ts
+++ src/Socket/chats.ts
@@ -780,7 +780,7 @@ export const makeChatsSocket = (config: SocketConfig) => {
 	/**
 	 * Star or Unstar a message
 	 */
-	const star = (jid, message, star) => {
+	const star = (jid: string, messages: [], star: boolean) => {
 	    return chatModify({
 	        star: {
 	            message,
