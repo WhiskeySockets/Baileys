c56a0a68b085f2dd6a406ca00ad3ab3769b1bec7
Laxeder <laxederbr@gmail.com>
2024-01-21 00:05:43 -0300

    fix: error opening connection

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -624,15 +624,20 @@ export const makeSocket = (config: SocketConfig) => {
 	})
 	// login complete
 	ws.on('CB:success', async(node: BinaryNode) => {
-		await uploadPreKeysToServerIfRequired()
-		await sendPassiveIq('active')
-
-		logger.info('opened connection to WA')
-		clearTimeout(qrTimer) // will never happen in all likelyhood -- but just in case WA sends success on first try
-
-		ev.emit('creds.update', { me: { ...authState.creds.me!, lid: node.attrs.lid } })
-
-		ev.emit('connection.update', { connection: 'open' })
+		try {
+			await uploadPreKeysToServerIfRequired()
+			await sendPassiveIq('active')
+			
+			logger.info('opened connection to WA')
+			clearTimeout(qrTimer) // will never happen in all likelyhood -- but just in case WA sends success on first try
+			
+			ev.emit('creds.update', { me: { ...authState.creds.me!, lid: node.attrs.lid } })
+			
+			ev.emit('connection.update', { connection: 'open' })
+		} catch (error) {
+			logger.info({ trace: error.stack }, 'error opening connection')
+			end(error)
+		}
 	})
 
 	ws.on('CB:stream:error', (node: BinaryNode) => {
