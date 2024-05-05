246ad4cb82fc5b20a34721dd11986ed6317927e9
Rajeh Taher <rajeh@reforward.dev>
2024-05-05 22:49:20 +0300

    comments

--- src/Store/make-in-memory-store.ts
+++ src/Store/make-in-memory-store.ts
@@ -178,7 +178,7 @@ export default (
 						const { user } = jidDecode(contactId)!
 						return [contactId, (await md5(Buffer.from(user + 'WA_ADD_NOTIF', 'utf8'))).toString('base64').slice(0, 3)]
 					}))
-					contact = contacts[contactHashes.find(([, b]) => b === update.id)?.[0] || ''] // find contact by attrs.hash
+					contact = contacts[contactHashes.find(([, b]) => b === update.id)?.[0] || ''] // find contact by attrs.hash, when user is not saved as a contact
 				}
 
 				if(contact) {
