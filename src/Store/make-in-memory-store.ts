b31ad35895ab61948cbb98e32537a9532dcb4927
alejandranhr <alejandranhr@gmail.com>
2024-05-05 11:51:45 -0500

    Update make-in-memory-store.ts
    fix error make-in-memory-store.js:159  Object.assign(contacts[update.id], contact);

--- src/Store/make-in-memory-store.ts
+++ src/Store/make-in-memory-store.ts
@@ -192,7 +192,7 @@ export default (
 					}
 				}
 
-				Object.assign(contacts[update.id!], contact)
+				Object.assign(contacts[update.id]!=undefined && contacts[update.id]!=null?contacts[update.id]:{}, contact);
 			}
 		})
 		ev.on('chats.upsert', newChats => {
