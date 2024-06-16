8b91f5fa51306d8f8a9dab8aba39398398832a5f
Bob <115008575+bobslavtriev@users.noreply.github.com>
2024-06-16 14:40:30 +0000

    lint

--- src/Store/make-in-memory-store.ts
+++ src/Store/make-in-memory-store.ts
@@ -137,7 +137,7 @@ export default (config: BaileysInMemoryStoreConfig) => {
 					const contactHashes = await Promise.all(Object.keys(contacts).map(async contactId => {
 						const { user } = jidDecode(contactId)!
 						return [contactId, (md5(Buffer.from(user + 'WA_ADD_NOTIF', 'utf8'))).toString('base64').slice(0, 3)]
-					})) 
+					}))
 					contact = contacts[contactHashes.find(([, b]) => b === update.id)?.[0] || ''] // find contact by attrs.hash, when user is not saved as a contact
 				}
 
