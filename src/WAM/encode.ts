045374c8ccfa653b77998e4e1fa92d8920b90e9f
Abdelrahman Tarek <y.p.challenges@gmail.com>
2024-07-28 13:33:21 +0300

    chore: Comment out unnecessary/forgotten console.log
    Unnecessary huge output being printed in the console

--- src/WAM/encode.ts
+++ src/WAM/encode.ts
@@ -11,7 +11,7 @@ export const encodeWAM = (binaryInfo: BinaryInfo) => {
 	encodeWAMHeader(binaryInfo)
 	encodeEvents(binaryInfo)
 
-	console.log(binaryInfo.buffer)
+	//console.log(binaryInfo.buffer)
 	const totalSize = binaryInfo.buffer
 		.map((a) => a.length)
 		.reduce((a, b) => a + b)
