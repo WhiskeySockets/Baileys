c02b772de0ad2e8b418a807a1fb174590d298c68
Cleiton Carvalho <cleitoncosta83@gmail.com>
2024-08-19 19:47:35 -0300

    fix: Fixed contacts.update when received changed picture event (#979)

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -414,7 +414,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 			const delPicture = getBinaryNodeChild(node, 'delete')
 
 			ev.emit('contacts.update', [{
-				id: jidNormalizedUser(node?.attrs?.jid) || ((setPicture || delPicture)?.attrs?.hash) || '',
+				id: jidNormalizedUser(node?.attrs?.from) || ((setPicture || delPicture)?.attrs?.hash) || '',
 				imgUrl: setPicture ? 'changed' : 'removed'
 			}])
 
