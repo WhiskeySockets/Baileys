47b2f6dd413e649eaf62b29ef7a0c69ed370b436
Rajeh Taher <rajeh@reforward.dev>
2024-09-28 14:26:54 +0300

    fix(master): Small fix to the profile picture endpoints

--- src/Socket/chats.ts
+++ src/Socket/chats.ts
@@ -221,13 +221,13 @@ export const makeChatsSocket = (config: SocketConfig) => {
 
 	/** update the profile picture for yourself or a group */
 	const updateProfilePicture = async(jid: string, content: WAMediaUpload) => {
-		let targetJid = ''
+		let targetJid;
 		if(!jid) {
 			throw new Boom('Illegal no-jid profile update. Please specify either your ID or the ID of the chat you wish to update')
 		}
 
 		if(jidNormalizedUser(jid) !== jidNormalizedUser(authState.creds.me!.id)) {
-			targetJid = jid // in case it is someone other than us
+			targetJid = jidNormalizedUser(jid) // in case it is someone other than us
 		}
 
 		const { img } = await generateProfilePicture(content)
@@ -251,13 +251,13 @@ export const makeChatsSocket = (config: SocketConfig) => {
 
 	/** remove the profile picture for yourself or a group */
 	const removeProfilePicture = async(jid: string) => {
-		let targetJid = ''
+		let targetJid;
 		if(!jid) {
 			throw new Boom('Illegal no-jid profile update. Please specify either your ID or the ID of the chat you wish to update')
 		}
 
 		if(jidNormalizedUser(jid) !== jidNormalizedUser(authState.creds.me!.id)) {
-			targetJid = jid // in case it is someone other than us
+			targetJid = jidNormalizedUser(jid) // in case it is someone other than us
 		}
 
 		await query({
