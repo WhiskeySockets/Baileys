b24f64f48761b7069090d5cb16825b4fbfcef886
NAME <mail@gmail.com>
2024-11-14 21:58:15 +0700

    removed lib

--- lib/Signal/libsignal.d.ts
+++
@@ -1,3 +0,0 @@
-import { SignalAuthState } from '../Types';
-import { SignalRepository } from '../Types/Signal';
-export declare function makeLibSignalRepository(auth: SignalAuthState): SignalRepository;
--- lib/Store/index.d.ts
+++
@@ -1,3 +0,0 @@
-import makeCacheManagerAuthState from './make-cache-manager-store';
-import makeInMemoryStore from './make-in-memory-store';
-export { makeInMemoryStore, makeCacheManagerAuthState };
