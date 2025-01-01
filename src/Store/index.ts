2ef2ff0d3860b0f64c56ef7ecea8e5c4d91f2c21
Monkey D Luffy <121231062+FaouzKK@users.noreply.github.com>
2025-01-01 12:30:06 +0000

    Update index.ts

--- src/Store/index.ts
+++ src/Store/index.ts
@@ -1,3 +1,4 @@
 import makeCacheManagerAuthState from './make-cache-manager-store'
 import makeInMemoryStore from './make-in-memory-store'
-export { makeInMemoryStore, makeCacheManagerAuthState }
+import  {makeInSQLiteStore} from './make-in-sql-store'
+export { makeInMemoryStore, makeCacheManagerAuthState, makeInSQLiteStore }
