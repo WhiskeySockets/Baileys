0b3fb7cacc6d2be7b58c792bbb93098055822816
gusta4321 <61188779+gusta4321@users.noreply.github.com>
2025-03-14 03:07:25 -0300

    Update Message.ts

--- src/Types/Message.ts
+++ src/Types/Message.ts
@@ -170,12 +170,14 @@ export type AnyRegularMessageContent = (
         listReply: Omit<proto.Message.IListResponseMessage, 'contextInfo'>
     }
     | {
-        pin: WAMessageKey
+        pin: {
+	key: WAMessageKey
         type: proto.PinInChat.Type
         /**
          * 24 hours, 7 days, 30 days
          */
         time?: 86400 | 604800 | 2592000
+	}
     }
     | {
         product: WASendableProduct
