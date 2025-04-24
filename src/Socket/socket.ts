6d4ce2cb18e7908cc70a1a59b577b673d5eddff7
canove <cassio@economicros.com.br>
2025-04-24 13:38:43 -0300

    chore: avoid unhandled promise timeout errors

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -218,7 +218,7 @@ export const makeSocket = (config: SocketConfig) => {
 
 		const [result] = await Promise.all([
 			waitForMessage(msgId, timeoutMs),
-			await sendNode(node)
+			sendNode(node)
 		])
 
 		if('tag' in result) {
