449ffd86430a430b06f5075030b6208a7a1a8417
Raphael Machado <raphael_primo@live.com>
2025-05-07 23:58:31 -0300

    adicionar BAILEYSBUSS_VERSION

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ export * from './WABinary'
 export * from './WAM'
 export * from './WAUSync'
 
+export const BAILEYSBUSS_VERSION = '7.5.2025'
+
 export type WASocket = ReturnType<typeof makeWASocket>
 export { makeWASocket }
 export default makeWASocket
