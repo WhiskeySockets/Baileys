fe3dbc81a59475fe8b110bde96f83f36bcb2a65d
vinikjkkj <viniwarrior4@gmail.com>
2025-05-15 13:27:15 -0300

    fix pairing code not working

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -28,7 +28,7 @@ export const Browsers: BrowsersMap = {
 
 export const getPlatformId = (browser: string) => {
 	const platformType = proto.DeviceProps.PlatformType[browser.toUpperCase()]
-	return platformType ? platformType.toString().charCodeAt(0).toString() : '49' //chrome
+	return platformType ? platformType.toString() : '1' //chrome
 }
 
 export const BufferJSON = {
