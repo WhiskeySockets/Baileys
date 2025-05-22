5627ecb6534fc9a77e750809ababb62d48e220f9
devlikepro <waha@devlike.pro>
2025-05-22 15:37:50 +0700

    Add lid to Contact - contact sync, history sync, group participants

--- src/Types/Contact.ts
+++ src/Types/Contact.ts
@@ -1,6 +1,10 @@
 export interface Contact {
+	/** ID either in lid or jid format **/
 	id: string
+	/** ID in Lid (anonymous) format (@lid) **/
 	lid?: string
+	/** ID in Phone Number format (@s.whatsapp.net)  **/
+	jid?: string
 	/** name of the contact, you have saved on your WA */
 	name?: string
 	/** name of the contact, the contact has set on their own on WA */
