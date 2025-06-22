73366f20add2d7e5f39d957efe07991a550dc662
Martin Schere <tinschere@gmail.com>
2025-06-22 11:39:04 +0200

    feat: 6.7.27

--- src/Types/Socket.ts
+++ src/Types/Socket.ts
@@ -16,9 +16,9 @@ export type CacheStore = {
     /** get a cached key and change the stats */
     get<T>(key: string): Promise<T> | T | undefined
     /** set a key in the cache */
-    set<T>(key: string, value: T): Promise<void> | void
+    set<T>(key: string, value: T): Promise<void> | void | number | boolean
     /** delete a key from the cache */
-    del(key: string): void | Promise<void>
+    del(key: string): void | Promise<void> | number | boolean
     /** flush all data */
     flushAll(): void | Promise<void>
 }
