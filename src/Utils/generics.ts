e62460cc4252fe29f787c64e16a85e29cf323b93
Stefan Lesicnik <stefan@snlr.us>
2025-07-22 07:12:40 +0400

    more ts way for version

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -3,8 +3,7 @@ import axios, { type AxiosRequestConfig } from 'axios'
 import { createHash, randomBytes } from 'crypto'
 import { platform, release } from 'os'
 import { proto } from '../../WAProto/index.js'
-import version from '../Defaults/baileys-version.json' with { type: 'json' }
-const baileysVersion = version.version
+import { version as baileysVersion } from '../Defaults/baileys-version.js'
 import type {
 	BaileysEventEmitter,
 	BaileysEventMap,
