1f2e687b510742f49b29aeac27fa76c70c9c549a
Paulo Victor Lund <pvictorlv@gmail.com>
2025-08-05 17:25:39 -0300

    mutex reimplementation

--- src/Signal/libsignal.ts
+++ src/Signal/libsignal.ts
@@ -1,24 +1,27 @@
 /* @ts-ignore */
 import * as libsignal from 'libsignal'
-import type { SignalAuthState } from '../Types'
-import type { SignalRepository } from '../Types/Signal'
-import { generateSignalPubKey } from '../Utils'
-import { jidDecode } from '../WABinary'
-import type { SenderKeyStore } from './Group/group_cipher'
-import { SenderKeyName } from './Group/sender-key-name'
-import { SenderKeyRecord } from './Group/sender-key-record'
-import { GroupCipher, GroupSessionBuilder, SenderKeyDistributionMessage } from './Group'
+import type {SignalAuthState, SignalKeyStoreWithTransaction} from '../Types'
+import type {SignalRepository} from '../Types/Signal'
+import {generateSignalPubKey} from '../Utils'
+import {jidDecode} from '../WABinary'
+import type {SenderKeyStore} from './Group/group_cipher'
+import {SenderKeyName} from './Group/sender-key-name'
+import {SenderKeyRecord} from './Group/sender-key-record'
+import {GroupCipher, GroupSessionBuilder, SenderKeyDistributionMessage} from './Group'
 
 export function makeLibSignalRepository(auth: SignalAuthState): SignalRepository {
 	const storage: SenderKeyStore = signalStorage(auth)
 	return {
-		decryptGroupMessage({ group, authorJid, msg }) {
+		decryptGroupMessage({group, authorJid, msg}) {
 			const senderName = jidToSignalSenderKeyName(group, authorJid)
 			const cipher = new GroupCipher(storage, senderName)
 
-			return cipher.decrypt(msg)
+			// Use transaction to ensure atomicity
+			return (auth.keys as SignalKeyStoreWithTransaction).transaction(async () => {
+				return cipher.decrypt(msg)
+			})
 		},
-		async processSenderKeyDistributionMessage({ item, authorJid }) {
+		async processSenderKeyDistributionMessage({item, authorJid}) {
 			const builder = new GroupSessionBuilder(storage)
 			if (!item.groupId) {
 				throw new Error('Group ID is required for sender key distribution message')
@@ -34,60 +37,79 @@ export function makeLibSignalRepository(auth: SignalAuthState): SignalRepository
 				item.axolotlSenderKeyDistributionMessage
 			)
 			const senderNameStr = senderName.toString()
-			const { [senderNameStr]: senderKey } = await auth.keys.get('sender-key', [senderNameStr])
+			const {[senderNameStr]: senderKey} = await auth.keys.get('sender-key', [senderNameStr])
 			if (!senderKey) {
 				await storage.storeSenderKey(senderName, new SenderKeyRecord())
 			}
 
-			await builder.process(senderName, senderMsg)
+			return (auth.keys as SignalKeyStoreWithTransaction).transaction(async () => {
+				const {[senderNameStr]: senderKey} = await auth.keys.get('sender-key', [senderNameStr])
+				if (!senderKey) {
+					await storage.storeSenderKey(senderName, new SenderKeyRecord())
+				}
+
+				await builder.process(senderName, senderMsg)
+			})
 		},
-		async decryptMessage({ jid, type, ciphertext }) {
+		async decryptMessage({jid, type, ciphertext}) {
 			const addr = jidToSignalProtocolAddress(jid)
 			const session = new libsignal.SessionCipher(storage, addr)
-			let result: Buffer
-			switch (type) {
-				case 'pkmsg':
-					result = await session.decryptPreKeyWhisperMessage(ciphertext)
-					break
-				case 'msg':
-					result = await session.decryptWhisperMessage(ciphertext)
-					break
-				default:
-					throw new Error(`Unknown message type: ${type}`)
-			}
 
-			return result
+			// Use transaction to ensure atomicity
+			return (auth.keys as SignalKeyStoreWithTransaction).transaction(async () => {
+				let result: Buffer
+				switch (type) {
+					case 'pkmsg':
+						result = await session.decryptPreKeyWhisperMessage(ciphertext)
+						break
+					case 'msg':
+						result = await session.decryptWhisperMessage(ciphertext)
+						break
+					default:
+						throw new Error(`Unknown message type: ${type}`)
+				}
+
+				return result
+			});
 		},
-		async encryptMessage({ jid, data }) {
+		async encryptMessage({jid, data}) {
 			const addr = jidToSignalProtocolAddress(jid)
 			const cipher = new libsignal.SessionCipher(storage, addr)
 
-			const { type: sigType, body } = await cipher.encrypt(data)
-			const type = sigType === 3 ? 'pkmsg' : 'msg'
-			return { type, ciphertext: Buffer.from(body, 'binary') }
+			// Use transaction to ensure atomicity
+			return (auth.keys as SignalKeyStoreWithTransaction).transaction(async () => {
+				const {type: sigType, body} = await cipher.encrypt(data)
+				const type = sigType === 3 ? 'pkmsg' : 'msg'
+				return {type, ciphertext: Buffer.from(body, 'binary')}
+			})
 		},
-		async encryptGroupMessage({ group, meId, data }) {
+		async encryptGroupMessage({group, meId, data}) {
 			const senderName = jidToSignalSenderKeyName(group, meId)
 			const builder = new GroupSessionBuilder(storage)
 
 			const senderNameStr = senderName.toString()
-			const { [senderNameStr]: senderKey } = await auth.keys.get('sender-key', [senderNameStr])
-			if (!senderKey) {
-				await storage.storeSenderKey(senderName, new SenderKeyRecord())
-			}
 
-			const senderKeyDistributionMessage = await builder.create(senderName)
-			const session = new GroupCipher(storage, senderName)
-			const ciphertext = await session.encrypt(data)
+			return (auth.keys as SignalKeyStoreWithTransaction).transaction(async () => {
+				const {[senderNameStr]: senderKey} = await auth.keys.get('sender-key', [senderNameStr])
+				if (!senderKey) {
+					await storage.storeSenderKey(senderName, new SenderKeyRecord())
+				}
 
-			return {
-				ciphertext,
-				senderKeyDistributionMessage: senderKeyDistributionMessage.serialize()
-			}
+				const senderKeyDistributionMessage = await builder.create(senderName)
+				const session = new GroupCipher(storage, senderName)
+				const ciphertext = await session.encrypt(data)
+
+				return {
+					ciphertext,
+					senderKeyDistributionMessage: senderKeyDistributionMessage.serialize()
+				}
+			});
 		},
-		async injectE2ESession({ jid, session }) {
+		async injectE2ESession({jid, session}) {
 			const cipher = new libsignal.SessionBuilder(storage, jidToSignalProtocolAddress(jid))
-			await cipher.initOutgoing(session)
+			return (auth.keys as SignalKeyStoreWithTransaction).transaction(async () => {
+				await cipher.initOutgoing(session)
+			});
 		},
 		jidToSignalProtocolAddress(jid) {
 			return jidToSignalProtocolAddress(jid).toString()
@@ -96,7 +118,7 @@ export function makeLibSignalRepository(auth: SignalAuthState): SignalRepository
 }
 
 const jidToSignalProtocolAddress = (jid: string) => {
-	const { user, device } = jidDecode(jid)!
+	const {user, device} = jidDecode(jid)!
 	return new libsignal.ProtocolAddress(user, device || 0)
 }
 
@@ -104,24 +126,24 @@ const jidToSignalSenderKeyName = (group: string, user: string): SenderKeyName =>
 	return new SenderKeyName(group, jidToSignalProtocolAddress(user))
 }
 
-function signalStorage({ creds, keys }: SignalAuthState): SenderKeyStore & Record<string, any> {
+function signalStorage({creds, keys}: SignalAuthState): SenderKeyStore & Record<string, any> {
 	return {
 		loadSession: async (id: string) => {
-			const { [id]: sess } = await keys.get('session', [id])
+			const {[id]: sess} = await keys.get('session', [id])
 			if (sess) {
 				return libsignal.SessionRecord.deserialize(sess)
 			}
 		},
 		// TODO: Replace with libsignal.SessionRecord when type exports are added to libsignal
 		storeSession: async (id: string, session: any) => {
-			await keys.set({ session: { [id]: session.serialize() } })
+			await keys.set({session: {[id]: session.serialize()}})
 		},
 		isTrustedIdentity: () => {
 			return true
 		},
 		loadPreKey: async (id: number | string) => {
 			const keyId = id.toString()
-			const { [keyId]: key } = await keys.get('pre-key', [keyId])
+			const {[keyId]: key} = await keys.get('pre-key', [keyId])
 			if (key) {
 				return {
 					privKey: Buffer.from(key.private),
@@ -129,7 +151,7 @@ function signalStorage({ creds, keys }: SignalAuthState): SenderKeyStore & Recor
 				}
 			}
 		},
-		removePreKey: (id: number) => keys.set({ 'pre-key': { [id]: null } }),
+		removePreKey: (id: number) => keys.set({'pre-key': {[id]: null}}),
 		loadSignedPreKey: () => {
 			const key = creds.signedPreKey
 			return {
@@ -139,7 +161,7 @@ function signalStorage({ creds, keys }: SignalAuthState): SenderKeyStore & Recor
 		},
 		loadSenderKey: async (senderKeyName: SenderKeyName) => {
 			const keyId = senderKeyName.toString()
-			const { [keyId]: key } = await keys.get('sender-key', [keyId])
+			const {[keyId]: key} = await keys.get('sender-key', [keyId])
 			if (key) {
 				return SenderKeyRecord.deserialize(key)
 			}
@@ -149,11 +171,11 @@ function signalStorage({ creds, keys }: SignalAuthState): SenderKeyStore & Recor
 		storeSenderKey: async (senderKeyName: SenderKeyName, key: SenderKeyRecord) => {
 			const keyId = senderKeyName.toString()
 			const serialized = JSON.stringify(key.serialize())
-			await keys.set({ 'sender-key': { [keyId]: Buffer.from(serialized, 'utf-8') } })
+			await keys.set({'sender-key': {[keyId]: Buffer.from(serialized, 'utf-8')}})
 		},
 		getOurRegistrationId: () => creds.registrationId,
 		getOurIdentity: () => {
-			const { signedIdentityKey } = creds
+			const {signedIdentityKey} = creds
 			return {
 				privKey: Buffer.from(signedIdentityKey.private),
 				pubKey: generateSignalPubKey(signedIdentityKey.public)
--- src/Utils/auth-utils.ts
+++ src/Utils/auth-utils.ts
@@ -1,6 +1,7 @@
 import NodeCache from '@cacheable/node-cache'
-import { randomBytes } from 'crypto'
-import { DEFAULT_CACHE_TTLS } from '../Defaults'
+import {randomBytes} from 'crypto'
+import {Mutex} from 'async-mutex'
+import {DEFAULT_CACHE_TTLS} from '../Defaults'
 import type {
 	AuthenticationCreds,
 	CacheStore,
@@ -10,9 +11,9 @@ import type {
 	SignalKeyStoreWithTransaction,
 	TransactionCapabilityOptions
 } from '../Types'
-import { Curve, signedKeyPair } from './crypto'
-import { delay, generateRegistrationId } from './generics'
-import type { ILogger } from './logger'
+import {Curve, signedKeyPair} from './crypto'
+import {delay, generateRegistrationId} from './generics'
+import type {ILogger} from './logger'
 
 /**
  * Adds caching capability to a SignalKeyStore
@@ -33,53 +34,239 @@ export function makeCacheableSignalKeyStore(
 			deleteOnExpire: true
 		})
 
+	// Mutex for protecting cache operations
+	const cacheMutex = new Mutex()
+
 	function getUniqueId(type: string, id: string) {
 		return `${type}.${id}`
 	}
 
 	return {
 		async get(type, ids) {
-			const data: { [_: string]: SignalDataTypeMap[typeof type] } = {}
-			const idsToFetch: string[] = []
-			for (const id of ids) {
-				const item = cache.get<SignalDataTypeMap[typeof type]>(getUniqueId(type, id)) as any
-				if (typeof item !== 'undefined') {
-					data[id] = item
-				} else {
-					idsToFetch.push(id)
-				}
-			}
-
-			if (idsToFetch.length) {
-				logger?.trace({ items: idsToFetch.length }, 'loading from store')
-				const fetched = await store.get(type, idsToFetch)
-				for (const id of idsToFetch) {
-					const item = fetched[id]
-					if (item) {
+			return cacheMutex.runExclusive(async () => {
+				const data: { [_: string]: SignalDataTypeMap[typeof type] } = {}
+				const idsToFetch: string[] = []
+				for (const id of ids) {
+					const item = cache.get<SignalDataTypeMap[typeof type]>(getUniqueId(type, id)) as any
+					if (typeof item !== 'undefined') {
 						data[id] = item
-						cache.set(getUniqueId(type, id), item)
+					} else {
+						idsToFetch.push(id)
 					}
 				}
-			}
 
-			return data
+				if (idsToFetch.length) {
+					logger?.trace({items: idsToFetch.length}, 'loading from store')
+					const fetched = await store.get(type, idsToFetch)
+					for (const id of idsToFetch) {
+						const item = fetched[id]
+						if (item) {
+							data[id] = item
+							cache.set(getUniqueId(type, id), item)
+						}
+					}
+				}
+
+				return data
+			})
 		},
 		async set(data) {
-			let keys = 0
-			for (const type in data) {
-				for (const id in data[type as keyof SignalDataTypeMap]) {
-					cache.set(getUniqueId(type, id), data[type as keyof SignalDataTypeMap]![id]!)
-					keys += 1
+			return cacheMutex.runExclusive(async () => {
+				let keys = 0
+				for (const type in data) {
+					for (const id in data[type as keyof SignalDataTypeMap]) {
+						cache.set(getUniqueId(type, id), data[type as keyof SignalDataTypeMap]![id]!)
+						keys += 1
+					}
 				}
-			}
 
-			logger?.trace({ keys }, 'updated cache')
+				logger?.trace({keys}, 'updated cache')
 
-			await store.set(data)
+				await store.set(data)
+			})
 		},
 		async clear() {
 			cache.flushAll()
 			await store.clear?.()
+		}
+	}
+}
+
+// Module-level specialized mutexes for pre-key operations
+const preKeyMutex = new Mutex()
+const signedPreKeyMutex = new Mutex()
+
+/**
+ * Get the appropriate mutex for the key type
+ */
+const getPreKeyMutex = (keyType: string): Mutex => {
+	return keyType === 'signed-pre-key' ? signedPreKeyMutex : preKeyMutex
+}
+
+/**
+ * Handles pre-key operations with mutex protection
+ */
+async function handlePreKeyOperations(
+	data: SignalDataSet,
+	keyType: keyof SignalDataTypeMap,
+	transactionCache: SignalDataSet,
+	mutations: SignalDataSet,
+	logger: ILogger,
+	isInTransaction: boolean,
+	state?: SignalKeyStore
+): Promise<void> {
+	const mutex = getPreKeyMutex(keyType)
+
+	await mutex.runExclusive(async () => {
+		const keyData = data[keyType]
+		if (!keyData) return
+
+		// Ensure structures exist
+		transactionCache[keyType] = transactionCache[keyType] || ({} as any)
+		mutations[keyType] = mutations[keyType] || ({} as any)
+
+		// Separate deletions from updates for batch processing
+		const deletionKeys: string[] = []
+		const updateKeys: string[] = []
+
+		for (const keyId in keyData) {
+			if (keyData[keyId] === null) {
+				deletionKeys.push(keyId)
+			} else {
+				updateKeys.push(keyId)
+			}
+		}
+
+		// Process updates first (no validation needed)
+		for (const keyId of updateKeys) {
+			if (transactionCache[keyType]) {
+				transactionCache[keyType][keyId] = keyData[keyId]!
+			}
+
+			if (mutations[keyType]) {
+				mutations[keyType][keyId] = keyData[keyId]!
+			}
+		}
+
+		// Process deletions with validation
+		if (deletionKeys.length === 0) return
+
+		if (isInTransaction) {
+			// In transaction, only allow deletion if key exists in cache
+			for (const keyId of deletionKeys) {
+				if (transactionCache && transactionCache[keyType]) {
+					transactionCache[keyType][keyId] = null
+					if (mutations[keyType]) {
+						// Mark for deletion in mutations
+						mutations[keyType][keyId] = null
+					}
+				} else {
+					logger.warn(`Skipping deletion of non-existent ${keyType} in transaction: ${keyId}`)
+				}
+			}
+
+			return
+		}
+
+		// Outside transaction, batch validate all deletions
+		if (!state) return
+
+		const existingKeys = await state.get(keyType as keyof SignalDataTypeMap, deletionKeys)
+		for (const keyId of deletionKeys) {
+			if (existingKeys[keyId]) {
+				if (transactionCache[keyType])
+					transactionCache[keyType][keyId] = null
+
+				if (mutations[keyType])
+					mutations[keyType][keyId] = null
+
+			} else {
+				logger.warn(`Skipping deletion of non-existent ${keyType}: ${keyId}`)
+			}
+		}
+	})
+}
+
+/**
+ * Handles normal key operations for transactions
+ */
+function handleNormalKeyOperations(
+	data: SignalDataSet,
+	key: keyof SignalDataTypeMap,
+	transactionCache: SignalDataSet,
+	mutations: SignalDataSet
+) {
+	Object.assign(transactionCache[key]!, data[key])
+	mutations[key] = mutations[key] || ({} as any)
+	Object.assign(mutations[key]!, data[key])
+}
+
+/**
+ * Process pre-key deletions with validation
+ */
+async function processPreKeyDeletions(
+	data: SignalDataSet,
+	keyType: keyof SignalDataTypeMap,
+	state: SignalKeyStore,
+	logger: ILogger
+): Promise<void> {
+	const mutex = getPreKeyMutex(keyType)
+
+	await mutex.runExclusive(async () => {
+		const keyData = data[keyType]
+		if (!keyData) return
+
+		// Validate deletions
+		for (const keyId in keyData) {
+			if (keyData[keyId] === null) {
+				const existingKeys = await state.get(keyType as keyof SignalDataTypeMap, [keyId])
+				if (!existingKeys[keyId]) {
+					logger.warn(`Skipping deletion of non-existent ${keyType}: ${keyId}`)
+
+					if (data[keyType])
+						delete data[keyType][keyId]
+				}
+			}
+		}
+	})
+}
+
+/**
+ * Executes a function with mutexes acquired for given key types
+ * Uses async-mutex's runExclusive with efficient batching
+ */
+async function withMutexes<T>(
+	keyTypes: string[],
+	getKeyTypeMutex: (type: string) => Mutex,
+	fn: () => Promise<T>
+): Promise<T> {
+	if (keyTypes.length === 0) {
+		return fn()
+	}
+
+	if (keyTypes.length === 1) {
+		return getKeyTypeMutex(keyTypes[0]!).runExclusive(fn)
+	}
+
+	// For multiple mutexes, sort by key type to prevent deadlocks
+	// Then acquire all mutexes in order using Promise.all for better efficiency
+	const sortedKeyTypes = [...keyTypes].sort()
+	const mutexes = sortedKeyTypes.map(getKeyTypeMutex)
+
+	// Acquire all mutexes in order to prevent deadlocks
+	const releases: (() => void)[] = []
+
+	try {
+		for (const mutex of mutexes) {
+			releases.push(await mutex.acquire())
+		}
+
+		return await fn()
+	} finally {
+		// Release in reverse order
+		while (releases.length > 0) {
+			const release = releases.pop()
+			if (release) release()
 		}
 	}
 }
@@ -94,7 +281,7 @@ export function makeCacheableSignalKeyStore(
 export const addTransactionCapability = (
 	state: SignalKeyStore,
 	logger: ILogger,
-	{ maxCommitRetries, delayBetweenTriesMs }: TransactionCapabilityOptions
+	{maxCommitRetries, delayBetweenTriesMs}: TransactionCapabilityOptions
 ): SignalKeyStoreWithTransaction => {
 	// number of queries made to the DB during the transaction
 	// only there for logging purposes
@@ -102,7 +289,46 @@ export const addTransactionCapability = (
 	let transactionCache: SignalDataSet = {}
 	let mutations: SignalDataSet = {}
 
+	// Mutex for each key type (session, pre-key, etc.)
+	const keyTypeMutexes = new Map<string, Mutex>()
+	// Per-sender-key-name mutexes for fine-grained serialization
+	const senderKeyMutexes = new Map<string, Mutex>()
+
 	let transactionsInProgress = 0
+
+	// Get or create a mutex for a specific key type
+	function getKeyTypeMutex(type: string): Mutex {
+		let mutex = keyTypeMutexes.get(type)
+		if (!mutex) {
+			// Create regular mutex, timeout only for critical operations
+			mutex = new Mutex()
+			keyTypeMutexes.set(type, mutex)
+		}
+
+		return mutex
+	}
+
+	// Get or create a mutex for a specific sender key name
+	function getSenderKeyMutex(senderKeyName: string): Mutex {
+		let mutex = senderKeyMutexes.get(senderKeyName)
+		if (!mutex) {
+			mutex = new Mutex()
+			senderKeyMutexes.set(senderKeyName, mutex)
+			logger.info({senderKeyName}, 'created new sender key mutex')
+		}
+
+		return mutex
+	}
+
+	// Sender key operations with proper mutex sequencing
+	function queueSenderKeyOperation<T>(senderKeyName: string, operation: () => Promise<T>): Promise<T> {
+		return getSenderKeyMutex(senderKeyName).runExclusive(operation)
+	}
+
+	// Check if we are currently in a transaction
+	function isInTransaction() {
+		return transactionsInProgress > 0
+	}
 
 	return {
 		get: async (type, ids) => {
@@ -112,10 +338,31 @@ export const addTransactionCapability = (
 				// only fetch if there are any items to fetch
 				if (idsRequiringFetch.length) {
 					dbQueriesInTransaction += 1
-					const result = await state.get(type, idsRequiringFetch)
 
-					transactionCache[type] ||= {}
-					Object.assign(transactionCache[type]!, result)
+					// Use per-sender-key queue for sender-key operations when possible
+					if (type === 'sender-key') {
+						logger.info({idsRequiringFetch}, 'processing sender keys in transaction')
+						// For sender keys, process each one with queued operations to maintain serialization
+						for (const senderKeyName of idsRequiringFetch) {
+							await queueSenderKeyOperation(senderKeyName, async () => {
+								logger.info({senderKeyName}, 'fetching sender key in transaction')
+								const result = await state.get(type, [senderKeyName])
+								// Update transaction cache
+								transactionCache[type] ||= {}
+								Object.assign(transactionCache[type]!, result)
+								logger.info({senderKeyName, hasResult: !!result[senderKeyName]}, 'sender key fetch complete')
+							})
+						}
+					} else {
+						// Use runExclusive for cleaner mutex handling
+						await getKeyTypeMutex(type as string).runExclusive(async () => {
+							const result = await state.get(type, idsRequiringFetch)
+
+							// Update transaction cache
+							transactionCache[type] ||= {}
+							Object.assign(transactionCache[type]!, result)
+						})
+					}
 				}
 
 				return ids.reduce((dict: { [T in string]: any }, id) => {
@@ -127,22 +374,99 @@ export const addTransactionCapability = (
 					return dict
 				}, {})
 			} else {
-				return state.get(type, ids)
+				// Not in transaction, fetch directly with queue protection
+				if (type === 'sender-key') {
+					// For sender keys, use individual queues to maintain per-key serialization
+					const results: { [key: string]: SignalDataTypeMap[typeof type] } = {}
+					for (const senderKeyName of ids) {
+						const result = await queueSenderKeyOperation(
+							senderKeyName,
+							async () => await state.get(type, [senderKeyName])
+						)
+						Object.assign(results, result)
+					}
+
+					return results
+				} else {
+					return await getKeyTypeMutex(type as string).runExclusive(() => state.get(type, ids))
+				}
 			}
 		},
-		set: data => {
+		set: async data => {
 			if (isInTransaction()) {
-				logger.trace({ types: Object.keys(data) }, 'caching in transaction')
+				logger.trace({types: Object.keys(data)}, 'caching in transaction')
 				for (const key_ in data) {
 					const key = key_ as keyof SignalDataTypeMap
 					transactionCache[key] = transactionCache[key] || ({} as any)
-					Object.assign(transactionCache[key]!, data[key])
 
-					mutations[key] = mutations[key] || ({} as any)
-					Object.assign(mutations[key]!, data[key])
+					// Special handling for pre-keys and signed-pre-keys
+					if (key === 'pre-key') {
+						await handlePreKeyOperations(data, key, transactionCache, mutations, logger, true)
+					} else {
+						// Normal handling for other key types
+						handleNormalKeyOperations(data, key, transactionCache, mutations)
+					}
 				}
 			} else {
-				return state.set(data)
+				// Not in transaction, apply directly with mutex protection
+				const hasSenderKeys = 'sender-key' in data
+				const senderKeyNames = hasSenderKeys ? Object.keys(data['sender-key'] || {}) : []
+
+				if (hasSenderKeys) {
+					logger.info({senderKeyNames}, 'processing sender key set operations')
+					// Handle sender key operations with per-key queues
+					for (const senderKeyName of senderKeyNames) {
+						await queueSenderKeyOperation(senderKeyName, async () => {
+							// Create data subset for this specific sender key
+							// const senderKeyData = {
+							// 	'sender-key': {
+							// 		[senderKeyName]: data['sender-key']![senderKeyName]
+							// 	}
+							// }
+
+							data['sender-key'] = data['sender-key'] || {}
+							data['sender-key'][senderKeyName] = data['sender-key']![senderKeyName] || null
+
+							logger.trace({senderKeyName}, 'storing sender key')
+							// Apply changes to the store
+							await state.set(data)
+							logger.trace({senderKeyName}, 'sender key stored')
+						})
+					}
+
+					// Handle any non-sender-key data with regular mutexes
+					const nonSenderKeyData = {...data}
+					delete nonSenderKeyData['sender-key']
+
+					if (Object.keys(nonSenderKeyData).length > 0) {
+						await withMutexes(Object.keys(nonSenderKeyData), getKeyTypeMutex, async () => {
+							// Process pre-keys and signed-pre-keys separately with specialized mutexes
+							for (const key_ in nonSenderKeyData) {
+								const keyType = key_ as keyof SignalDataTypeMap
+								if (keyType === 'pre-key') {
+									await processPreKeyDeletions(nonSenderKeyData, keyType, state, logger)
+								}
+							}
+
+							// Apply changes to the store
+							await state.set(nonSenderKeyData)
+						})
+					}
+				} else {
+					// No sender keys - use original logic
+					await withMutexes(Object.keys(data), getKeyTypeMutex, async () => {
+						// Process pre-keys and signed-pre-keys separately with specialized mutexes
+						for (const key_ in data) {
+							const keyType = key_ as keyof SignalDataTypeMap
+							if (keyType === 'pre-key') {
+								await processPreKeyDeletions(data, keyType, state, logger)
+							}
+						}
+
+						// Apply changes to the store
+						await state.set(data)
+					})
+				}
 			}
 		},
 		isInTransaction,
@@ -167,7 +491,7 @@ export const addTransactionCapability = (
 							//eslint-disable-next-line max-depth
 							try {
 								await state.set(mutations)
-								logger.trace({ dbQueriesInTransaction }, 'committed transaction')
+								logger.trace({dbQueriesInTransaction}, 'committed transaction')
 								break
 							} catch (error) {
 								logger.warn(`failed to commit ${Object.keys(mutations).length} mutations, tries left=${tries}`)
@@ -189,10 +513,6 @@ export const addTransactionCapability = (
 
 			return result
 		}
-	}
-
-	function isInTransaction() {
-		return transactionsInProgress > 0
 	}
 }
 
