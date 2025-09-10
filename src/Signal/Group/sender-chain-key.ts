a1f063dc048aa710bb3b3037665939e81494f537
αѕтяσχ11 <devastro0010@gmail.com>
2025-09-10 19:51:29 +0100

    fix: handle chain key objects for skmsg group message decryption
    previously, some sender chain keys were stored or retrieved as plain objects
    (e.g. { '0': 85, '1': 100, ... }) instead of Buffers. this caused
    "ERR_INVALID_ARG_TYPE" during initial decryption of skmsg group messages.
    
    this fixes any chain key object is converted to a proper Buffer
    before being passed to SenderChainKey.

--- src/Signal/Group/sender-chain-key.ts
+++ src/Signal/Group/sender-chain-key.ts
@@ -11,7 +11,6 @@ export class SenderChainKey {
 	constructor(iteration: number, chainKey: any) {
 		this.iteration = iteration
 		if (Buffer.isBuffer(chainKey)) {
-			// backported from @MartinSchere's PR
 			this.chainKey = chainKey
 		} else if (chainKey instanceof Uint8Array) {
 			this.chainKey = Buffer.from(chainKey)
