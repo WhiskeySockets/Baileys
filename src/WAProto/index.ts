7296414de31e229b9e99a8ec76934258dde749ec
Raphael Serafim <raphaelvserafim@gmail.com>
2025-09-09 21:36:00 -0400

    feat: add WAProto module with proto export and type definition

---
+++ src/WAProto/index.ts
@@ -0,0 +1,3 @@
+import $root from '@raphaelvserafim/whatsapp-proto/dist/index.js'
+export const proto = $root.proto
+export type { proto as ProtoType } from '@raphaelvserafim/whatsapp-proto/dist/index.d.ts'
