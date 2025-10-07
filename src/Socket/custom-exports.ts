5ea5bf610fd512f92fdb2be2deb6d848dbf76746
xvannnDev <rahmatdany038@gmail.com>
2025-10-07 20:23:35 +0700

    Create custom-exports.ts

---
+++ src/Socket/custom-exports.ts
@@ -0,0 +1,26 @@
+export const waChatKey = (chat: any) => chat?.id || ''
+
+export const MessageTypeProto = {
+  text: 'conversation',
+  image: 'imageMessage',
+  video: 'videoMessage',
+}
+
+export const WANode = {}
+export const WAFlag = {}
+export const MiscMessageGenerationOptions = {}
+export const ChatModification = {}
+export const WALocationMessage = {}
+export const ProxyAgent = {}
+export const MediaConnInfo = {}
+export const URL_REGEX = /https?:\/\/[^\s]+/
+export const WAUrlInfo = {}
+export const MimetypeMap = {}
+export const MediaPathMap = {}
+export const WAContactMessage = {}
+export const WAContactsArrayMessage = {}
+export const WAGroupInviteMessage = {}
+export const WATextMessage = {}
+export const templateMessage = {}
+export const InteractiveMessage = {}
+export const Header = {}
