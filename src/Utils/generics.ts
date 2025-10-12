48b0cb4bc2b16901ea325203f6132629642c845e
Rajeh Taher <rajeh@reforward.dev>
2025-10-12 19:50:04 +0300

    chore: update wa web ver

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -1,7 +1,7 @@
 import { Boom } from '@hapi/boom'
 import { createHash, randomBytes } from 'crypto'
 import { proto } from '../../WAProto/index.js'
-const baileysVersion = [2, 3000, 1023223821]
+const baileysVersion = [2, 3000, 1027934701]
 import type {
 	BaileysEventEmitter,
 	BaileysEventMap,
