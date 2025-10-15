a2468b60bb4c4099baf82350ef6b1eba85a30fc7
João Lucas <jlucaso@hotmail.com>
2025-10-15 12:09:20 -0300

    fix: increase encoding buffer size to process videos correctly

--- src/WABinary/encode.ts
+++ src/WABinary/encode.ts
@@ -1,7 +1,7 @@
 import { encodeNodeTo } from 'whatsapp-rust-bridge/binary'
 import type { BinaryNode } from './types'
 
-const encodingBuffer = new Uint8Array(8192)
+const encodingBuffer = new Uint8Array(16384)
 
 export const encodeBinaryNode = (node: BinaryNode): Uint8Array => {
 	const bytesWritten = encodeNodeTo(node, encodingBuffer)
