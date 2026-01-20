d36d9c194c64e5d84976b6e97536a7870935c389
David ??? <86541514+DavidModzz@users.noreply.github.com>
2026-01-20 07:41:33 -0300

    Add groupStatusMessage checks in message handling (#2258)

--- src/Utils/messages.ts
+++ src/Utils/messages.ts
@@ -782,7 +782,9 @@ export const normalizeMessageContent = (content: WAMessageContent | null | undef
 			message?.documentWithCaptionMessage ||
 			message?.viewOnceMessageV2 ||
 			message?.viewOnceMessageV2Extension ||
-			message?.editedMessage
+			message?.editedMessage ||
+			message?.groupStatusMessage ||
+			message?.groupStatusMessageV2
 		)
 	}
 }
