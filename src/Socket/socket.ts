b24212638c234fd21cd7275d2490f03caf12014b
qikerrs <129662698+qikerrs@users.noreply.github.com>
2026-01-31 02:05:27 +0700

    Make end function asynchronous for closing socket

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -607,7 +607,7 @@ export const makeSocket = (config: SocketConfig) => {
 		})
 	}
 
-	const end = (error: Error | undefined) => {
+	const end = async (error: Error | undefined) => {
 		if (closed) {
 			logger.trace({ trace: error?.stack }, 'connection already closed')
 			return
@@ -625,7 +625,7 @@ export const makeSocket = (config: SocketConfig) => {
 
 		if (!ws.isClosed && !ws.isClosing) {
 			try {
-				ws.close()
+				await ws.close()
 			} catch {}
 		}
 
