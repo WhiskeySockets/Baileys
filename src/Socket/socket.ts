2e667769abe3cda3184dd56a53e9d86a5423c407
João Lucas <jlucaso@hotmail.com>
2026-03-31 23:20:23 -0300

    feat: add companion registration utilities and refactor QR data generation

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -21,12 +21,14 @@ import {
 	aesEncryptCTR,
 	bindWaitForConnectionUpdate,
 	bytesToCrockford,
+	buildPairingQRData,
 	configureSuccessfulPairing,
 	Curve,
 	derivePairingCodeKey,
 	generateLoginNode,
 	generateMdTagPrefix,
 	generateRegistrationNode,
+	getCompanionPlatformId,
 	getCodeFromWSError,
 	getErrorCodeFromStreamError,
 	getNextPreKeysNode,
@@ -36,7 +38,6 @@ import {
 	signedKeyPair,
 	xmppSignedPreKey
 } from '../Utils'
-import { getPlatformId } from '../Utils/browser-utils'
 import {
 	assertNodeErrorFree,
 	type BinaryNode,
@@ -790,7 +791,7 @@ export const makeSocket = (config: SocketConfig) => {
 						{
 							tag: 'companion_platform_id',
 							attrs: {},
-							content: getPlatformId(browser[1])
+							content: getCompanionPlatformId(browser)
 						},
 						{
 							tag: 'companion_platform_display',
@@ -883,7 +884,7 @@ export const makeSocket = (config: SocketConfig) => {
 			}
 
 			const ref = (refNode.content as Buffer).toString('utf-8')
-			const qr = [ref, noiseKeyB64, identityKeyB64, advB64].join(',')
+			const qr = buildPairingQRData(ref, noiseKeyB64, identityKeyB64, advB64, browser)
 
 			ev.emit('connection.update', { qr })
 
---
+++ src/Utils/companion-reg-client-utils.ts
@@ -0,0 +1,45 @@
+import type { WABrowserDescription } from '../Types'
+
+export enum CompanionWebClientType {
+	UNKNOWN = 0,
+	CHROME = 1,
+	EDGE = 2,
+	FIREFOX = 3,
+	IE = 4,
+	OPERA = 5,
+	SAFARI = 6,
+	ELECTRON = 7,
+	UWP = 8,
+	OTHER_WEB_CLIENT = 9
+}
+
+const BROWSER_TO_COMPANION_WEB_CLIENT: Record<string, CompanionWebClientType> = {
+	Chrome: CompanionWebClientType.CHROME,
+	Edge: CompanionWebClientType.EDGE,
+	Firefox: CompanionWebClientType.FIREFOX,
+	IE: CompanionWebClientType.IE,
+	Opera: CompanionWebClientType.OPERA,
+	Safari: CompanionWebClientType.SAFARI
+}
+
+export const getCompanionWebClientType = ([os, browserName]: WABrowserDescription): CompanionWebClientType => {
+	if (browserName === 'Desktop') {
+		return os === 'Windows' ? CompanionWebClientType.UWP : CompanionWebClientType.ELECTRON
+	}
+
+	return BROWSER_TO_COMPANION_WEB_CLIENT[browserName] || CompanionWebClientType.OTHER_WEB_CLIENT
+}
+
+export const getCompanionPlatformId = (browser: WABrowserDescription): string => {
+	return getCompanionWebClientType(browser).toString()
+}
+
+export const buildPairingQRData = (
+	ref: string,
+	noiseKeyB64: string,
+	identityKeyB64: string,
+	advB64: string,
+	browser: WABrowserDescription
+): string => {
+	return [ref, noiseKeyB64, identityKeyB64, advB64, getCompanionPlatformId(browser)].join(',')
+}
