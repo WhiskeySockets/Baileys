f9f012f61c75f936144c35d8c69e745487b5394d
kaikybrofc <kaikygomesribeiroof@gmail.com>
2026-05-02 19:33:17 +0000

    style(socket): fix lint formatting in chats and messages-recv

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -288,6 +288,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 							mappings.push(mapping)
 						}
 					}
+
 					await signalRepository.lidMapping.storeLIDPNMappings(mappings)
 				}
 
@@ -1524,14 +1525,14 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 				status
 			}
 
-      if (status === 'relaylatency') {
-        const latencyValue = infoChild.attrs.latency || infoChild.attrs['latency_ms'] || infoChild.attrs['latency-ms']
-        const latencyMs = latencyValue ? Number(latencyValue) : undefined
-        if (Number.isFinite(latencyMs)) {
-          call.latencyMs = latencyMs
-        }
-      }
-      
+			if (status === 'relaylatency') {
+				const latencyValue = infoChild.attrs.latency || infoChild.attrs['latency_ms'] || infoChild.attrs['latency-ms']
+				const latencyMs = latencyValue ? Number(latencyValue) : undefined
+				if (Number.isFinite(latencyMs)) {
+					call.latencyMs = latencyMs
+				}
+			}
+
 			if (status === 'offer') {
 				call.isVideo = !!getBinaryNodeChild(infoChild, 'video')
 				call.isGroup = infoChild.attrs.type === 'group' || !!infoChild.attrs['group-jid']
@@ -1659,6 +1660,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 			)
 			ignoreJid = !isNodeFromMe || isJidGroup(attrs.from) ? attrs.from : attrs.recipient
 		}
+
 		if (ignoreJid && ignoreJid !== S_WHATSAPP_NET && shouldIgnoreJid(ignoreJid)) {
 			await sendMessageAck(node, type === 'message' ? NACK_REASONS.UnhandledError : undefined)
 			return
